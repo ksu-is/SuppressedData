2f59d6395250359fe07a8042464d4b4bab50839d
Joshua Austen Trowell <54424388+JoshuaTrowell@users.noreply.github.com>
2019-11-18 13:15:19 -0500

    Add files via upload

---
+++ SuppressedData.py
@@ -0,0 +1,15 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #set the map document we're working on
+mxd = arcpy.mapping.MapDocument(r"Trowell_GunViolence.mxd")
+#set the dataframe and layer we're searching in. 2nd parameter in df is the dataframe you want to work in, enter empty string
+#for default df; 2nd parameter in lyr is the layer you want to work in. Case sensitive!
+df = arcpy.mapping.ListDataFrames(mxd, "DeathsbyFirearm")[0]   
+lyr = arcpy.mapping.ListLayers(mxd, "SoutheasternStates", df)[0]
+
+#create feature layer (temp layer) to copy to; 1st parameter input layer, 2nd parameter output layer
+arcpy.MakeFeatureLayer_management("Features.gdb/SoutheasternStates", "SuppressedData_lyr")
+#select by attribute
+arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "Gun_Violence = Suppressed")
+#selection is made, time to copy to new layer from selection
+arcpy.CopyFeatures_management("SuppressedData_lyr", "Features.gdb/Crude_Ra_1")
