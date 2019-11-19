f9474e1b21b16611743befb44ec77d98e2cb4c9b
Joshua Austen Trowell <54424388+JoshuaTrowell@users.noreply.github.com>
2019-11-19 17:46:40 -0500

    Update SuppressedData.py

--- SuppressedData.py
+++ SuppressedData.py
@@ -6,8 +6,8 @@ df = arcpy.mapping.ListDataFrames(mxd, "DeathsbyFirearm")[0]
 lyr = arcpy.mapping.ListLayers(mxd, "SoutheasternStates", df)[0]
 
 #create feature layer (temp layer) to copy to; 1st parameter input layer, 2nd parameter output layer
-arcpy.MakeFeatureLayer_management("Features.gdb/SoutheasternStates", "SuppressedData_lyr")
+arcpy.MakeFeatureLayer_management("G:/Features.gdb/SoutheasternStates", "Suppressed_lyr")
 #select by attribute
-arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "Crude_Ra_1 = Suppressed")
+arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "\"Deaths\" = \'Supressed\'")
 #selection is made, time to copy to new layer from selection
-arcpy.CopyFeatures_management("SuppressedData_lyr", "Features.gdb/Crude_Ra_1")
+arcpy.CopyFeatures_management("Suppressed_lyr", "Features.gdb/Deaths")
